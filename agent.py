503842ed54f731308655c2a336a139df8c6cee96
rich legrand <rich@charmedlabs.com>
2020-08-28 15:26:21 -0500

    added mutex lock

--- agent.py
+++ agent.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request, render_template
+from threading import Lock
 import json
 import subprocess
 
 app = Flask(__name__)
+lock = Lock()
 
 def run_python(code, timeout=5, maxlen=5000):
     proc = subprocess.Popen(['python3'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -14,7 +16,7 @@ def run_python(code, timeout=5, maxlen=5000):
     else:
         out = out.decode('utf-8')
         if len(out)>maxlen:
-            return "Sorry, you program's output is too lengthy!"
+            return "Sorry, your program's output is too lengthy!"
         return out + err.decode('utf-8')
 
 
@@ -25,9 +27,10 @@ def index():
 @app.route('/post_data', methods=['POST'])
 def post_data():
     if request.method == "POST":
-        code = json.loads(request.data)
-        out = run_python(code)
-        return Response(json.dumps(out), mimetype="application/json")
-        
+        with lock:
+            code = json.loads(request.data)
+            out = run_python(code)
+            return Response(json.dumps(out), mimetype="application/json")
+
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0")
